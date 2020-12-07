4e373d37c1dd6800983ef3b887376945f108179b
Diogo Castro <diogo.castro@cern.ch>
2020-12-07 23:29:20 +0000

    Reload if server says so

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 $(document).ready(function() {
 	if (OC.currentUser !== false) {
 		$.getJSON("/index.php/apps/canary", function(data) {
-			if (data["is_adopter"]) {
+			if (data["reload"] || data["is_adopter"]) {
 				location.reload();
 			}
 		});
